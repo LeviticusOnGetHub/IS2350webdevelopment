07cfe7baedf0a15678701cd4c7d8d7ca31384e47
Leviticus Land <lland01@indianatech.net>
2026-09-21 19:41:51 -0400

    react app setup

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,7 +1,16 @@
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ textAlign: "center", padding: "2rem" }}>
       <h1>Hello World!</h1>
+      <h1>Leviticus Land </h1>
+
+      <img
+        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fmedia1.tenor.com%2Fm%2Ft273D3EqiIYAAAAC%2Fone-piece-luffy.gif&f=1&nofb=1&ipt=b8d5292174caf2886a961a8338c6f66b980b599631cc6d83c152567f388771a5"
+        alt="Fat Luffy"
+        style={{ maxWidth: "400px", width: "100%", borderRadius: "12px", display: "block", margin: "0 auto 2rem" }}
+      />
+
+
     </div>
   );
 }
